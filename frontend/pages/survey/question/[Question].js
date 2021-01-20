594ea2a1595e82fb88b0323abaf0717fb4667566
Ossi <ossi.j.lehtonen@helsinki.fi>
2021-01-20 16:54:12 +0200

    eslint setup

--- frontend/pages/survey/question/[Question].js
+++ frontend/pages/survey/question/[Question].js
@@ -1,7 +1,9 @@
+import React from 'react';
 import { useRouter } from 'next/router';
 
 const Question = () => {
   const router = useRouter();
+  // eslint-disable-next-line no-console
   console.log('💩 ~ file: [Question].js ~ line 5 ~ router', router);
   return <section>[Insert great survey question...]</section>;
 };
