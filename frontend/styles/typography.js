f3e4d64a537b8b5935c6d599c2900497448f0f9d
Joel <joel.nhn@gmail.com>
2021-01-21 17:30:00 +0200

    add typography

---
+++ frontend/styles/typography.js
@@ -0,0 +1,19 @@
+export const fontSizes = [
+  null,
+  '.75rem',
+  '.875rem',
+  '1rem',
+  '1.125rem',
+  '1.25rem',
+  '1.5rem',
+  '1.75rem',
+  '2.25rem',
+  '3rem',
+  '4rem',
+];
+
+export const fonts = {
+  sourceSerifPro: '"Source Serif Pro", "sans-serif";',
+};
+
+export const lineHeights = [null, 1, 1.25, 1.5, 1.8];
