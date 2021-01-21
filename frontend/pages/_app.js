15d498096174dd13dfa6a7fabb5d1b659870aa26
Joel <joel.nhn@gmail.com>
2021-01-21 17:29:50 +0200

    make global styles available to app tree

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,9 +1,15 @@
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable react/prop-types */
 import React from 'react';
+import { GlobalStyles } from '../styles/global';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
