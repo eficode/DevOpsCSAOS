d5963d0b7b33ac5c57725bc44a4ace4b3d531ee1
Ossi <ossi.j.lehtonen@helsinki.fi>
2021-01-24 18:37:56 +0200

    delete broken test that uses db

--- backendv2/tests/db.test.js
+++
@@ -1,8 +0,0 @@
-const request = require('supertest')
-const app = require('../app')
-describe('User API', () => {
-    it('should show all users', async () => {
-        const res = await request(app).get('/answers')
-        expect(res.body).toHaveProperty('answers')
-    }),
-{}})
