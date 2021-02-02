791a0e548136059e16e6a5a66615210b859f21f6
Joel <joel.nhn@gmail.com>
2021-02-02 21:43:26 +0200

    fetch questions at app level

--- frontend/pages/_app.jsx
+++ frontend/pages/_app.jsx
@@ -1,11 +1,16 @@
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable react/prop-types */
-import React from 'react';
-import { ThemeProvider } from 'styled-components';
-import GlobalStyles from '../styles/global';
-import theme from '../styles/theme';
+import React from 'react'
+import { ThemeProvider } from 'styled-components'
+import getAll from '../services/questions'
+import { useStore } from '../store'
+import GlobalStyles from '../styles/global'
+import theme from '../styles/theme'
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, questions }) {
+  // set the questions to state as we transition to client-side
+  useStore.setState({ questions: questions })
+
   return (
     <>
       <GlobalStyles />
@@ -13,7 +18,13 @@ function MyApp({ Component, pageProps }) {
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  );
+  )
 }
 
-export default MyApp;
+MyApp.getInitialProps = async () => {
+  // fetch all questions and pass them to top-level app
+  const questions = await getAll()
+  return { questions }
+}
+
+export default MyApp
---
+++ frontend/pages/survey/questions/[questionId].jsx
@@ -0,0 +1,88 @@
+import React, { useState } from 'react'
+import { useRouter } from 'next/router'
+import styled from 'styled-components'
+
+import ContentWrapper from '../../../components/contentWrapper'
+import Button from '../../../components/button'
+import Option from '../../../components/option'
+import { useStore } from '../../../store'
+
+const OptionsWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 100px 100px 100px;
+  grid-template-rows: 60px 60px;
+  column-gap: 10px;
+  row-gap: 15px;
+  margin-top: 20px;
+  margin-bottom: 20px;
+`
+
+const Heading = styled.h3`
+  color: ${({ theme }) => theme.colors.blueDianne};
+`
+
+const QuestionTitle = styled.h2`
+  color: ${({ theme }) => theme.colors.blueDianne};
+`
+
+const Question = () => {
+  const [selectedValue, setSelectedValue] = useState(0)
+  const router = useRouter()
+
+  const questions = useStore((state) => state.questions)
+
+  const questionId = Number(router.query.questionId)
+  const nextQuestionUrl = `/survey/${questionId + 1}`
+  const isLast = questionId === questions.length
+
+  const Buttons = isLast ? (
+    <Button type="submit" onClick={() => console.log('saving!')}>
+      <span>Get results!</span>
+    </Button>
+  ) : (
+    <Button type="button" onClick={() => router.push(nextQuestionUrl)}>
+      <span>Next</span>
+    </Button>
+  )
+
+  return (
+    <ContentWrapper>
+      <Heading>DevOps Assessment Tool</Heading>
+      <span>
+        {' '}
+        Question {questionId}/{questions.length}{' '}
+      </span>
+      <QuestionTitle>{questions[questionId - 1].text}</QuestionTitle>
+      <OptionsWrapper>
+        <Option
+          label="Strongly agree"
+          selected={selectedValue === 5}
+          onClick={() => setSelectedValue(5)}
+        />
+        <Option
+          label="Agree"
+          selected={selectedValue === 4}
+          onClick={() => setSelectedValue(4)}
+        />
+        <Option
+          label="Neutral"
+          selected={selectedValue === 3}
+          onClick={() => setSelectedValue(3)}
+        />
+        <Option
+          label="Disagree"
+          selected={selectedValue === 2}
+          onClick={() => setSelectedValue(2)}
+        />
+        <Option
+          label="Strongly disagree"
+          selected={selectedValue === 1}
+          onClick={() => setSelectedValue(1)}
+        />
+      </OptionsWrapper>
+      {Buttons}
+    </ContentWrapper>
+  )
+}
+
+export default Question
