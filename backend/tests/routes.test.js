8fe47d559c74022703df419d629bdce0dea65285
Ossi <ossi.j.lehtonen@helsinki.fi>
2021-02-02 09:24:34 +0200

    refactor tests and add node as test env

--- backend/tests/routes.test.js
+++ backend/tests/routes.test.js
@@ -1,14 +1,27 @@
 const request = require('supertest')
 const app = require('../app.js')
+const { initDatabase } = require('../config/setupDatabase')
 
 describe('GET /api/questions', () => {
   it('has four questions', async (done) => {
-    const response = await request(app).get('/api/questions')
-    expect(response.status).toBe(200)
-    expect(response.body).toHaveLength(4)
-    done()
-  })
-  afterAll(async () => {
-    await new Promise((resolve) => setTimeout(() => resolve(), 500)) // wait for 500 ms for async tasks to finish
-  })
+    await initDatabase()
+    const response = await request(app).get('/api/questions');
+    expect(response.body).toHaveLength(4);
+    done();
+  });
+
 })
+
+describe('GET /api/question/:id', () => {
+  it('Returns correct data with valid id', async (done) => {
+    
+    const response = await request(app).get('/api/question/1')
+    expect(response.status).toBe(200)
+    const question = response.body
+    
+    expect(question.text).toBe("Oletko ruisleipä?");
+    expect(question.weight).toBe(0.8);
+    expect(question.categoryId).toBe(1);
+    done();
+  });
+});
