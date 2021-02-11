263e44799f8ae11567bc1549b1b251e272fcb636
Joel <joel.nhn@gmail.com>
2021-02-11 16:36:17 +0200

    run prettier

--- frontend/services/constants.js
+++ frontend/services/constants.js
@@ -1,3 +1,4 @@
-export const HOST = process.env.NODE_ENV === 'production'
-  ? 'https://ohtu-csaos-staging.herokuapp.com'
-  : 'http://localhost:3000';
+export const HOST =
+  process.env.NODE_ENV === 'production'
+    ? 'https://ohtu-csaos-staging.herokuapp.com'
+    : 'http://localhost:3000'
