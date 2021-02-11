31ee91d5101df36dd7d0af9f6e547ab459ce82f3
Joel <joel.nhn@gmail.com>
2021-02-11 16:35:17 +0200

    summary now shows questions and answers

--- frontend/pages/survey/questions/summary.jsx
+++ frontend/pages/survey/questions/summary.jsx
@@ -1,19 +1,50 @@
+import Link from 'next/link'
 import React from 'react'
+import styled from 'styled-components'
 import { useStore } from '../../../store'
+
+const QuestionAnswerWrapper = styled.article`
+  margin: 1rem 0;
+
+  span {
+    display: inline-block;
+    margin: 0.4rem 0;
+  }
+`
+
+function getKeyByValue(object, value) {
+  return Object.keys(object).find((key) => object[key] === value)
+}
 
 const Summary = () => {
   const selections = useStore((state) => state.selections)
   const optionsToPointsMap = useStore((state) => state.optionsToPointsMap)
   const questions = useStore((state) => state.questions)
-  console.log('💩 ~ file: summary.jsx ~ line 8 ~ questions', questions)
 
-  console.log(
-    '💩 ~ file: summary.jsx ~ line 7 ~ optionsToPointsMap',
-    optionsToPointsMap
+  return (
+    <div>
+      <h2>Here's what you've answered</h2>
+      {questions.map((question, index) => {
+        let answerToQuestion = getKeyByValue(
+          optionsToPointsMap,
+          selections[index]
+        ).toLowerCase()
+
+        if (answerToQuestion === 'neutral') {
+          answerToQuestion = 'feel neutral'
+        }
+
+        return (
+          <QuestionAnswerWrapper>
+            <span>Question: {question.text}</span>
+            <br />
+            <span>You {answerToQuestion}</span>
+          </QuestionAnswerWrapper>
+        )
+      })}
+      <Link href="/survey/result">Go to your results!</Link>
+    </div>
   )
-  console.log('💩 ~ file: summary.jsx ~ line 6 ~ selections', selections)
-
-  return <div>This is a summary of your answers</div>
 }
 
 export default Summary
