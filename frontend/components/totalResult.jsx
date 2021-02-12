1dd0a843386bc8e9f16d5a3d3907496e8822e4a4
Johanna <jespoof@outlook.com>
2021-02-12 20:57:17 +0200

    renamed result component for clarity (renamed component now included)

---
+++ frontend/components/totalResult.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const ResultCircle = styled.div`
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+  font-size: 32px;
+  background: ${({ theme }) => theme.colors.gold};
+  text-align: center;
+  position: relative;
+`
+
+export const Result = styled.span`
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  -ms-transform: translate(-50%, -50%);
+  -webkit-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+`
+
+const TotalResult = ({ userResult, maxResult }) => (
+  <ResultCircle><Result>{userResult}/{maxResult}</Result></ResultCircle>
+)
+
+export default TotalResult
