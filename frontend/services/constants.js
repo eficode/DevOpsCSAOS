b73b2b15ea803948449105babe3994bdb61c555f
Ossi <ossi.j.lehtonen@helsinki.fi>
2021-02-25 09:46:38 +0200

    add build:dev script and API_URL env variable

--- frontend/services/constants.js
+++ frontend/services/constants.js
@@ -1,3 +1,22 @@
-export const HOST = process.env.NODE_ENV === 'production'
-  ? 'https://ohtu-csaos-staging.herokuapp.com'
-  : 'http://localhost:3000'
+const getHost = () => {
+
+  if ( process.env.NODE_ENV === 'development' ) {
+    const PORT = process.env.PORT
+    /*
+      This might be misleading, 
+      but the server.js file rewrites all /api requests to process.env.API_URL 
+      (default is localhost:5000 when npm run dev is run)
+
+      The reason for this is that this fixes cors policy error when running frontend.
+    */
+    return `http://localhost:${PORT}`
+  }
+  /*
+      This is used when build is created
+  */
+  return process.env.API_URL
+}
+
+export const HOST = getHost()
+
+
