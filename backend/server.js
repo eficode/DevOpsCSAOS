141590fe83b72581124dc498b4326aa686930920
Riikka Korolainen <riikka.korolainen@outlook.com>
2021-02-26 16:00:41 +0200

    txt file with e2e test case plan, removed initial data insertion to test db in backend

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,19 @@
 /* eslint-disable prettier/prettier */
 const app = require('./app')
 
+const { PORT, NODE_ENV } = process.env
+
 const { initDatabase } = require('./config/setupDatabase')
 
-const port = process.env.NODE_ENV === 'test' ? 5001 : 
-  process.env.PORT || 5000
+const port = NODE_ENV === 'test' ? 5001 : 
+  PORT || 5000
 
 app.listen({ port }, async () => {
-  await initDatabase()
+  
+  if (NODE_ENV !== 'test') {
+    await initDatabase()
+  }
+  
   console.log('database initialized')
   console.log(`'Server up on http://localhost:${port}`)
 })
