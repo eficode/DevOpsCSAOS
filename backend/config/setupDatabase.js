ecd627fbd2877fe19af5ccae747e4d4f24ebb908
Riikka Korolainen <riikka.korolainen@outlook.com>
2021-03-01 10:57:21 +0200

    removed jsons from backend

--- backend/config/setupDatabase.js
+++ backend/config/setupDatabase.js
@@ -4,10 +4,6 @@ const users = require('./initialData/users.json')
 const answers = require('./initialData/answers.json')
 const results = require('./initialData/results.json')
 
-const categoriesTestData = require('./initialTestData/categories.json')
-const questionsTestData = require('./initialTestData/questions.json')
-const resultsTestData = require('./initialTestData/results.json')
-
 const {
   sequelize,
   Question,
@@ -19,24 +15,17 @@ const {
 
 /*
   currently app has to be started in test mode before running robot.
-  This ensures that app has data during build-time before robot
+  Following code ensures that app has data during build-time before robot
   inserts seed data (testData copied from seed_database.sql)
 */
-const initDatabase = process.env.NODE_ENV === 'test' ? 
-async () => {
-  await sequelize.sync({ force: true })
-  await Category.bulkCreate(categoriesTestData)
-  await Question.bulkCreate(questionsTestData)
-  await Result.bulkCreate(resultsTestData)
-}
-:
-async () => {
-  await sequelize.sync({ force: true })
-  await Category.bulkCreate(categories)
-  await User.bulkCreate(users)
-  await Question.bulkCreate(questions)
-  await Answer.bulkCreate(answers)
-  await Result.bulkCreate(results)
-}
+const initDatabase =
+  async () => {
+    await sequelize.sync({ force: true })
+    await Category.bulkCreate(categories)
+    await User.bulkCreate(users)
+    await Question.bulkCreate(questions)
+    await Answer.bulkCreate(answers)
+    await Result.bulkCreate(results)
+  }
 
 module.exports = { initDatabase }
--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,9 @@ const port = NODE_ENV === 'test' ? 5001 :
   PORT || 5000
 
 app.listen({ port }, async () => {
+  if (NODE_ENV !== 'test'){
     await initDatabase()
+  }
   
   console.log('database initialized')
   console.log(`'Server up on http://localhost:${port}`)
