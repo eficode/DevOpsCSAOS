ddfbc770ca60feac6b9b00b98a2e72c1a12bfe57
Ossi <ossi.j.lehtonen@helsinki.fi>
2021-03-02 15:40:07 +0200

    test endtoend tests workflow

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 /* eslint-disable prettier/prettier */
 const app = require('./app')
-
+const {
+  sequelize,
+} = require('./models')
 const { PORT, NODE_ENV } = process.env
 
 const { initDatabase } = require('./config/setupDatabase')
@@ -9,10 +11,14 @@ const port = NODE_ENV === 'endtoend' ? 5001 :
   PORT || 5000
 
 app.listen({ port }, async () => {
-  if (NODE_ENV !== 'endtoend'){
+  if (NODE_ENV ==='endtoend'){
+    // create all database tables
+    await sequelize.sync({ force: true })
+  } else {
     await initDatabase()
+    console.log('database initialized')
   }
   
-  console.log('database initialized')
+  
   console.log(`'Server up on http://localhost:${port}`)
 })
