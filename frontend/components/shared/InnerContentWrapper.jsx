2a0b06b4e179c7a2e4c1aaf93a3ace2a60021058
Joel <joel.nhn@gmail.com>
2021-03-18 14:35:16 +0200

    fix weird bug

--- frontend/components/shared/InnerContentWrapper.jsx
+++ frontend/components/shared/InnerContentWrapper.jsx
@@ -1,7 +1,9 @@
 import styled from 'styled-components'
 
-
-export const InnerContentWrapper = styled.div`
+/**
+ * Don't change to a div! Will not work (unknown reason -.-). 
+ * */
+export const InnerContentWrapper = styled.section`
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -16,31 +18,7 @@ export const InnerContentWrapper = styled.div`
   border-radius: 0.5rem;
   /* flex: 1; */
   
-  .MuiButtonBase-root{
-    font-family: Montserrat;
-    font-weight: bold;
-    font-size: 14px;
-    text-decoration: none;
-    margin: 10px;
-    min-width: 120px;
-    height: 45px;
-    text-align:center;
-    line-height: 45px;
-    border-radius: 5px;
-    border-width: 0px;
-    margin: 50px 25px 15px 25px;
-    padding: 0 20px;
-    text-transform: none;
 
-    cursor:pointer;
-
-    background-color: ${({ theme }) => theme.colors.blueDianne};
-    color: white;
-
-    &:hover {
-      background-color: ${({ theme }) => theme.colors.easternBlue};
-    }
-  }
 
 `
 export default InnerContentWrapper
