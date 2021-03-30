b367c933d193d89c5ed90cc2171ebd0db0699fff
Ossi Lehtonen <ossi.j.lehtonen@helsinki.fi>
2021-03-30 10:34:03 +0300

    Delete unused endpoint
    Co-authored-by: korolainenriikka <55230880+korolainenriikka@users.noreply.github.com>
    Co-authored-by: johannaspo <41202016+johannaspo@users.noreply.github.com>

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -12,17 +12,6 @@ usersRouter.post('/', async (req, res) => {
   }
 })
 
-// get by email
-usersRouter.get('/:email', async (req, res) => {
-  const { email } = req.params
-  try {
-    const users = await User.findOne({
-      where: { email },
-    })
-    return res.json(users)
-  } catch (e) {
-    return res.status(500).json({ error: 'Unable to fetch users' })
-  }
-})
+
 
 module.exports = usersRouter
