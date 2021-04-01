05f48a6fac8fb4e03fb4ed6e0c2804e66fbb1f9d
Joel <joel.nhn@gmail.com>
2021-04-01 12:24:56 +0300

    button refactor
    Co-authored-by: jp-tulijoki <56557413+jp-tulijoki@users.noreply.github.com>

--- frontend/components/button.jsx
+++ frontend/components/button.jsx
@@ -1,91 +1,37 @@
 import React from 'react'
-// import styled from 'styled-components'
-import { withStyles } from '@material-ui/core/styles'
+import styled from 'styled-components'
 import { Button as MUIButton } from '@material-ui/core'
-import theme from '../styles/theme'
 
-const StyledButton = withStyles({
-  root: {
-    backgroundColor: theme.colors.blueDianne,
-    color: 'white',
+const StyledButton = styled(MUIButton)`
+  background-color: ${({ theme }) => theme.colors.blueDianne};
+  color: white;
 
-    '&:hover': {
-      backgroundColor: theme.colors.easternBlue,
-    },
-
-    fontFamily: 'Montserrat',
-    fontWeight: 'bold',
-    fontSize: '14px',
-    textDecoration: 'none',
-    borderRadius: '5px',
-    borderWidth: '0px',
-    marginTop: '40px',
-    minWidth: '120px',
-    lineHeight: '45px',
-    paddingLeft: '10px',
-    paddingRight: '10px',
-    textTransform: 'capitalize',
-
-    cursor: 'pointer',
-
-    '.MuiTouchRipple-root': {
-      display: 'none',
-    },
-  },
-})(MUIButton)
-
-/* const ButtonWrapper = styled.div`
-  .MuiButtonBase-root {
-    background-color: ${({ theme }) => theme.colors.blueDianne};
-    color: white;
-
-    &:hover {
-      background-color: ${({ theme }) => theme.colors.easternBlue};
-    }
-
-    font-family: Montserrat;
-    font-weight: bold;
-    font-size: 14px;
-    text-decoration: none;
-    border-radius: 5px;
-    border-width: 0px;
-    margin-top: 40px;
-    min-width: 120px;
-    line-height: 45px;
-    padding-left: 10px;
-    padding-right: 10px;
-    text-transform: capitalize;
-
-    cursor: pointer;
-
-    .MuiTouchRipple-root {
-      display: none;
-    }
-  }
-` */
-const Button = ({ children, type, onClick }) => {
-  if (type === 'button') {
-    return (
-      // <ButtonWrapper>
-      <StyledButton onClick={onClick} type="button">
-        {children}
-      </StyledButton>
-      // </ButtonWrapper>
-    )
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.easternBlue};
   }
 
-  if (type === 'submit') {
-    return (
-      // <ButtonWrapper>
-      <StyledButton onClick={onClick} type="submit">
-        {children}
-      </StyledButton>
-      // </ButtonWrapper>
-    )
-  }
+  font-family: Montserrat;
+  font-weight: bold;
+  font-size: 14px;
+  text-decoration: none;
+  border-radius: 5px;
+  border-width: 0px;
+  margin-top: 40px;
+  min-width: 120px;
+  line-height: 45px;
+  padding-left: 10px;
+  padding-right: 10px;
+  text-transform: capitalize;
 
-  console.warn('custom button only has button and submit types')
-  return <></>
+  cursor: pointer;
+`
+
+const Button = ({ onClick, children, type }) => {
+  return (
+    <StyledButton onClick={onClick} type={type}>
+      {children}
+    </StyledButton>
+  )
 }
 
 export default Button
