ddb82162fc496ce0de493a85e4939a4a1c5285be
Johanna <jespoof@outlook.com>
2021-04-12 16:00:46 +0300

    styled detailed results form
    Co-authored-by: lehtoneo <48444546+lehtoneo@users.noreply.github.com>

--- frontend/components/getDetailedResultsForm.jsx
+++ frontend/components/getDetailedResultsForm.jsx
@@ -1,21 +1,25 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
 import { isEmail } from 'validator'
-import Checkbox from '@material-ui/core/Checkbox'
-
+import Checkbox from '@material-ui/core/Checkbox' 
+import IndustrySelector from './industrySelector'
 import Button from '../components/button'
-import IndustrySelector from '../components/industrySelector'
+import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined'
+import IconButton from '@material-ui/core/IconButton'
 
 const FormBackGround = styled.div`
-  width: 80%;
+  width: 85%;
   margin-top: 30px;
   padding: 15px;
+  background: #99C2D0;
+  border-radius: 20px;
 `
 
 const FormTitle = styled.h3`
   color: ${({ theme }) => theme.colors.blueDianne};
   font-family: Merriweather;
   margin: 10px;
+  padding-top: 30px;
 `
 
 const DetailsForm = styled.form`
@@ -32,19 +36,63 @@ const DetailsInput = styled.input`
   border-radius: 10px;
   border-width: 0px;
   margin: 0 0 10px 0;
+  ::placeholder {
+    color: #000;
+  }
+  color: #000;
+  width: 100%;
 `
 
-const GroupLinkCheckBoxWrapper = styled.div`
+const CheckboxContainer = styled.div`
+  margin-top: 10px;
+
+  a {
+    font-weight: bold;
+    text-decoration: underline;
+  }
+`
+
+const FieldWrapper = styled.div`
+  flex-direction: column;
+  justify-content: flex-start;
+  width: 100%;
+  padding: 0 10%;
+`
+
+const StyledCheckbox = styled(Checkbox)`
+  padding: 0 5px 0 0;
+`
+
+const StyledIconButton = styled(IconButton)`
+  padding: 3px;
+  color: rgb(30, 57, 68);
 `
 
 const CheckBoxText = styled.label`
   font-size: 13px;
 `
 
+const Info = styled.div`
+  display: ${props => props.open ? "block" : "none"};
+  width: 130px;
+  height: 70px;
+  background-color: ${({ theme }) => theme.colors.whiteSmoke};
+  position: absolute;
+  left: 46%;
+  z-index: 1;
+  font-size: 12px;
+  padding: 15px;
+  border-radius: 10px;
+`
+const StyledIcon = styled(InfoOutlinedIcon)`
+  
+`
+
 const GetDetailedResultsForm = () => {
   const [emailInput, setEmailInput] = useState('')
-  const [checked, setChecked] = useState(true);
-
+  const [createGroupChecked, setCreateGroupChecked] = useState(false)
+  const [agreeToPrivacyPolicyChecked, setAgreeToPrivacyPolicyChecked] = useState(false)
+  const [infoOpen, setInfoOpen] = useState(false)
   const handleEmailChange = (event) => {
     event.preventDefault()
     setEmailInput(event.target.value)
@@ -60,41 +108,61 @@ const GetDetailedResultsForm = () => {
     }
   }
 
-  const handleChange = (event) => {
-    setChecked(event.target.checked);
-  };
+  const handleCreateGroupChange = (event) => {
+    setCreateGroupChecked(event.target.checked)
+  }
+
+  const handleAgreeToPolicyChange = (event) => {
+    setAgreeToPrivacyPolicyChecked(event.target.checked)
+  }
   
   return (
     <FormBackGround>
       <FormTitle>Get your detailed results</FormTitle>
-      <DetailsForm id="email-input-field" onSubmit={updateEmail}>
-        <DetailsInput
-          id="email"
-          name="email"
-          placeholder="Email"
-          value={emailInput}
-          onChange={handleEmailChange}
-          required
-        />
-        {emailInput &&
-        <>
-          <GroupLinkCheckBoxWrapper>
-            <Checkbox
-              checked={checked}
-              onChange={handleChange}
-              name="checked"
-              style ={{
-                color: "#1E3944",
-              }}
+      
+        <DetailsForm id="email-input-field" onSubmit={updateEmail}>
+          <FieldWrapper>
+            <DetailsInput
+              id="email"
+              name="email"
+              placeholder="Email"
+              value={emailInput}
+              onChange={handleEmailChange}
+              required
             />
-            <CheckBoxText>Create a group</CheckBoxText>
-          </GroupLinkCheckBoxWrapper>
-          <IndustrySelector />
-        </>}
-        <Button id="submit-email-button" type="submit">
-          Mail me!
-        </Button>
-      </DetailsForm>
+            
+            <IndustrySelector />
+              <CheckboxContainer>
+                <StyledCheckbox
+                  checked={createGroupChecked}
+                  onChange={handleCreateGroupChange}
+                  name="checked"
+                  style ={{
+                    color: "#1E3944",
+                  }}
+                />
+                <CheckBoxText>Create a group</CheckBoxText>
+                <StyledIconButton aria-label="info" component="span" onClick={() => setInfoOpen(!infoOpen)} >
+                  <StyledIcon style={{ fontSize: 20 }}/>
+                </StyledIconButton>
+                <Info open={infoOpen}>Jotain</Info>
+              </CheckboxContainer>
+              <CheckboxContainer>
+                <StyledCheckbox
+                  checked={agreeToPrivacyPolicyChecked}
+                  onChange={handleAgreeToPolicyChange}
+                  name="checked"
+                  style ={{
+                    color: "#1E3944",
+                  }}
+                />
+                <CheckBoxText>Agree to the <a>privacy policy</a></CheckBoxText>
+              </CheckboxContainer>
+              </FieldWrapper>
+            <Button id="submit-email-button" type="submit">
+              Submit
+            </Button>
+        </DetailsForm>
     </FormBackGround>
   )
 }
--- frontend/components/industrySelector.jsx
+++ frontend/components/industrySelector.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import styled from 'styled-components'
 import {
   InputLabel,
   MenuItem,
@@ -6,28 +7,54 @@ import {
   Select
 } from '@material-ui/core'
 
-const IndustrySelector = () => {
-  const [industry, setIndustry] = useState('')
+const StyledFormControl = styled(FormControl)`
+  background-color: ${({ theme }) => theme.colors.whiteSmoke};
+  font-family: Montserrat;
+  border-radius: 10px;
+  border-width: 0px;
+  padding: 4.5px 5px;
+  margin-bottom: 10px;
+  min-width: 100%;
+`
+const StyledSelect = styled(Select)`
+  font-family: Montserrat;
+  font-size: 11px;
+  padding-left: 15px;
+  background-color: ${({ theme }) => theme.colors.whiteSmoke};
+  color: ${props => props.value === 0 ? "#717171" : "#000000"};
+  .MuiSelect-select:focus {
+    background-color: ${({ theme }) => theme.colors.whiteSmoke};
+  }
+`
 
+const StyledMenuItem = styled(MenuItem)`
+  font-family: Montserrat;
+  font-size: 14px;
+`
+
+const IndustrySelector = () => {
+  const [industry, setIndustry] = useState(0)
   const handleChange = (event) => {
     setIndustry(event.target.value)
   }
 
   return (
-    <FormControl>
-      <InputLabel >
-        Field?
-      </InputLabel>
-      <Select
+    <StyledFormControl>
+      <StyledSelect 
+        disableUnderline
+        displayEmpty
         value={industry}
         onChange={handleChange}
       >
-        <MenuItem value={1}>Software</MenuItem>
-        <MenuItem value={2}>Malware</MenuItem>
-        <MenuItem value={3}>Warfare</MenuItem>
-        <MenuItem value={4}>Other</MenuItem>
-      </Select>
-    </FormControl>
+        <StyledMenuItem value={0} disabled>
+          Select industry
+        </StyledMenuItem>
+        <StyledMenuItem value={1}>Software</StyledMenuItem>
+        <StyledMenuItem value={2}>Malware</StyledMenuItem>
+        <StyledMenuItem value={3}>Warfare</StyledMenuItem>
+        <StyledMenuItem value={4}>Other</StyledMenuItem>
+      </StyledSelect>
+    </StyledFormControl>
     )
 }
     
