48af5579d457811c781490de6631cc1f833966e4
Joel <joel.nhn@gmail.com>
2021-04-15 15:13:56 +0300

    create hubspot client and controller
    Co-authored-by: jp-tulijoki <56557413+jp-tulijoki@users.noreply.github.com>

--- backend/app.js
+++ backend/app.js
@@ -8,10 +8,12 @@ app.use(express.static(path.join(__dirname, '../frontend/out')))
 const questionsRouter = require('./controllers/questions')
 const usersRouter = require('./controllers/users')
 const answersRouter = require('./controllers/answers')
+const hubspotRouter = require('./controllers/hubspot')
 
 app.use('/api/questions', questionsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/answers', answersRouter)
+app.use('/api/hubspot', hubspotRouter)
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/out/index.html'))
