0027cb4cc4bd5e4e80fe7a41777a0624728aefea
Joel <joel.nhn@gmail.com>
2021-04-27 14:21:27 +0300

    clean up inner content wrapper

--- frontend/components/shared/InnerContentWrapper.jsx
+++ frontend/components/shared/InnerContentWrapper.jsx
@@ -1,29 +1,15 @@
 import styled from 'styled-components'
 
 export const InnerContentWrapper = styled.section`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
+  display: grid;
+  place-items: center;
 
-  align-items: center;
   width: 90%;
-
   height: auto;
-  min-height: 100%;
-  min-height: 25rem;
-
-  @media screen and (max-width: ${({ theme }) => theme.breakpoints.wideMobile}) {
-    min-height: 20;
-  }
-
-  @media screen and (max-width: ${({ theme }) => theme.breakpoints.mediumMobile}) {
-    position: absolute;
-    height: auto;
-    min-height: 100%;
-  }
+  min-height: 30rem;
 
   margin: 0;
-  padding: 7% 0;
+  padding: 7%;
   background-color: white;
   border-radius: 0.5rem;
 `
